8cde2a11be76a88f5d8279fd7eccb36f427be9c9
SandraQS <sanquesan@gmail.com>
2021-11-10 22:26:57 +0100

    Create loginUser function at usersControllers

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -1,16 +1,51 @@
+require("dotenv").config();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../database/models/users");
 
-const createUser = async (req, res) => {
-  const { name, username, password, admin } = req.body;
-
-  const newUser = await User.create({
-    name,
-    username,
-    password: await bcrypt.hash(password, 10),
-    admin,
-  });
-  res.json(newUser);
+const createUser = async (req, res, next) => {
+  try {
+    const { name, username, password, admin } = req.body;
+    const newUser = await User.create({
+      name,
+      username,
+      password: await bcrypt.hash(password, 10),
+      admin,
+    });
+    res.json(newUser);
+  } catch (error) {
+    error.code = 400;
+    error.message = "Formato no válido";
+    next(error);
+  }
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      const error = new Error("Algo ha fallado");
+      error.code = 401;
+      next(error);
+    } else {
+      const passwordExist = await bcrypt.compare(password, user.password);
+      if (!passwordExist) {
+        const error = new Error("Algo ha fallado");
+        error.code = 401;
+        next(error);
+      } else {
+        const token = jwt.sign({ user, id: user.id }, process.env.SECRET_TOKEN);
+        res.json({ token });
+      }
+    }
+  } catch {
+    const error = new Error("No autorizado");
+    error.code = 401;
+    next(error);
+  }
+};
+
+module.exports = { createUser, loginUser };
--- server/index.js
+++ server/index.js
@@ -2,20 +2,16 @@ const morgan = require("morgan");
 const debug = require("debug")("series:server");
 const express = require("express");
 const chalk = require("chalk");
+const cors = require("cors");
 
 const {
   notFoundErrorHandler,
   generalErrorHandler,
 } = require("./middlewares/error");
 
-const cors = require("cors");
 const usersRoutes = require("./routes/usersRoutes");
 
-
-
 const seriesRoutes = require("./routes/seriesRoutes");
-
-
 
 const app = express();
 
@@ -42,15 +38,9 @@ const initializerServer = (port) =>
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-
-
+app.use("/users", usersRoutes);
+app.use("/series", seriesRoutes);
 app.use(notFoundErrorHandler);
 app.use(generalErrorHandler);
-
-app.use("/users", usersRoutes);
-
-app.use("/series", seriesRoutes);
-
-
 
 module.exports = { initializerServer };
