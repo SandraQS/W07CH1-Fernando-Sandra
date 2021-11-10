75ab019a8dbc9de995e4c352ed99344714e63776
SandraQS <sanquesan@gmail.com>
2021-11-10 19:12:13 +0100

    Modify import initializerServer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const initDB = require("./database/index");
-const initializerServer = require("./server/index");
+const { initializerServer } = require("./server/index");
 
 const port = process.env.PORT ?? process.env.SERVER_PORT ?? 3005;
 
