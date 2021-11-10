f72fde9054fede31d3e83fe07932bdd0ee23eecd
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-10 19:07:43 +0100

    Add index server file with initializerServer function

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const morgan = require("morgan");
+const debug = require("debug")("series:server");
+const express = require("express");
+const chalk = require("chalk");
+
+const app = express();
+
+const initializerServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.yellow(`Escuchando en el puerto ${port}.`));
+      resolve(server);
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("Error al iniciar el servidor."));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`El puerto ${port} ya está ocupado`));
+      }
+      reject();
+    });
+
+    server.on("close", () => {
+      debug(chalk.yellow("El servidor express está desconectado"));
+    });
+  });
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+module.exports = { initializerServer };
